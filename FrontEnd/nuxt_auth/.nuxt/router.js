6560e6b3cfe36a3cedb8c91904a9dd715dd11f0d
Pedro Collin <pedrocollintkd@gmail.com>
2022-02-01 11:30:01 -0300

    inplement

---
+++ FrontEnd/nuxt_auth/.nuxt/router.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _c4f2384c = () => interopDefault(import('..\\pages\\login.vue' /* webpackChunkName: "pages/login" */))
+const _529e6ec2 = () => interopDefault(import('..\\pages\\register.vue' /* webpackChunkName: "pages/register" */))
+const _58417e7a = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/login",
+    component: _c4f2384c,
+    name: "login"
+  }, {
+    path: "/register",
+    component: _529e6ec2,
+    name: "register"
+  }, {
+    path: "/",
+    component: _58417e7a,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
---
+++ FrontEnd/nuxt_auth/pages/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent="submit">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <input v-model="username" type="text" class="form-control" placeholder="Usuario" required>
+
+    <input v-model="password" type="password" class="form-control" placeholder="Password" required>
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    async submit() {
+      await fetch('http://127.0.0.1:8000/api/v1/auth/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include',
+        body: JSON.stringify({
+          username: this.username,
+          password: this.password
+        })
+      });
+
+      await this.$router.push('/');
+    }
+  }
+}
+</script>
+
